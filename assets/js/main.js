caafb7db0bb2276b9c1018cd5bb6723c6f1316d4
thyago-asp <57697311+thyago-asp@users.noreply.github.com>
2020-12-01 10:56:31 -0300

    Atualização do fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,9 @@
 /* 2. sticky And Scroll UP */
     $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
-      if (scroll < 400) {
+      if (scroll < 130) {
         $(".header-sticky").removeClass("sticky-bar");
-        $('#back-top').fadeOut(500);
+        $('#back-top').fadeOut(1000);
       } else {
         $(".header-sticky").addClass("sticky-bar");
         $('#back-top').fadeIn(500);
