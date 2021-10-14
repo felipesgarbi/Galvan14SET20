95cea6741d9a1c902fcc93267b6d902c405dd599
felipesgarbi <70644231+felipesgarbi@users.noreply.github.com>
2021-10-14 09:17:57 -0300

    post #12

---
+++ desmistificando-o-cub/index.php
@@ -0,0 +1,290 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/estrutura/head.php"; ?>
+
+<body>
+   <!--? Preloader Start -->
+   <div id="preloader-active">
+      <div class="preloader d-flex align-items-center justify-content-center">
+         <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+               <img src="../assets/img/logo-galvan/logo-galvan.png" alt="logo-galvan-engenharia">
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- Preloader Start -->
+   <?php include $_SERVER["DOCUMENT_ROOT"] . "/estrutura/header.php"; ?>
+   <main>
+
+      <!-- TOP DO BLOG INICIO -->
+      <div class="col-md-12 border mb-5">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 font-ruda textoCentralizado pt-40 mb-5">
+                  <h2><b class="textoAzul">BLOG DA GALVAN</b></h2>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- TOP DO BLOG FIM -->
+
+      <!--? Blog Area Start -->
+      <section class="blog_area single-post-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-8 posts-list">
+                  <div class="single-post">
+                        <h5>14/10/2021 - Por: Galvan Engenharia</h5>
+                     <div class="feature-img">
+                        <img class="img-fluid" src="../desmistificando-o-cub/CAPA.png" alt="desmistificando-o-cub-capa"/>
+                     </div>
+                     <div class="blog_details">
+                        <h2 style="color: #2d2d2d; text-align:justify;">
+                        <b>Desmistificando o CUB</b></h2>
+                        
+                        <!--<div class="quote-wrapper">
+                           <div class="quotes">
+                           </br>Mesmo que a edificação seja nova, é comum o surgimento de pequenas "cicatrizes" nas paredes. Isso ocorre porque parte da construção ainda está se acomodando.
+                           </br>Mas então como saber quando devo me preocupar?
+                           </br>Vem com a gente entender um pouco mais sobre FISSURAS, TRINCAS e RACHADURAS.
+                           </div>
+                        </div>-->
+
+                        <span>
+                        Conheça o índice aplicado na compra de imóveis na planta e como ele vai impactar no valor final do seu imóvel. 
+                           <p>CUB - Custo Unitário Básico da Construção Civil</p>
+                           </br>O CUB leva em consideração fatores como variação de preço dos insumos, equipamentos e mão de obra para diferentes padrões de construção.
+                           </br>Seu cálculo é regional, variando de estado para estado, e o responsável pelo cálculo é o SINDUSCON de cada estado.
+                           <a>https://sinduscon-fpolis.org.br/</a>
+                           <p>Esse índice é utilizado como indexador contratual na compra de imóveis na planta.</p>
+                           </br>Ele permite que a construtora pratique preços de venda menores ao longo da obra, sem ter que onerar o cliente estimando um aumento de custo durante o longo período da construção.
+                           <p>A correção do CUB é aplicada sobre o saldo DEVEDOR, e não sobre o valor total do imóvel.</p>
+                           </br>Por isso, quanto maior o valor da entrada e das parcelas pagas, menor a incidência de correção.
+                           <p>Como calcular o reajuste do CUB sobre as parcelas do meu imóvel?</p>
+                        </span>
+
+                        <h2 class="pt-40" style="color: #2d2d2d;">1. Primeiro você deve transformar todo valor do contrato em CUB.</h2>
+                        <p style="text-align:justify;">
+                           
+                        </p>
+                        
+                        <div class="feature-img">
+                           <img class="img-fluid" src="../desmistificando-o-cub/img1.png" style="max-width:100%;" alt="valor-contrato-cub"/>
+                        </div>
+
+                        <h2 class="pt-40" style="color: #2d2d2d;">2. Suponhamos que você pagou uma entrada de R$100.000,00 na assinatura do contrato.</h2>
+                        <p style="text-align:justify;">
+                           Seu saldo devedor agora é de: 226,83 - 45,39 = 181,54 CUBs.
+                        </p>
+                        
+                        <div class="feature-img">
+                           <img class="img-fluid" src="../desmistificando-o-cub/img2.png" style="max-width:100%;" alt="conversao-em-cubs"/>
+                        </div>
+                        
+                        <h2 class="pt-40" style="color: #2d2d2d;">3. Imagine que você pretende pagar prestações de R$3.000,00 ao longo de 20 meses da obra</h2>
+                        <p style="text-align:justify;">
+                        O valor em reais a ser pago mensalmente irá varia de acordo com o CUB. Ex.: em junho/21 o valor da parcela será: 1,36 * R$2.307,92 (CUB de jun/21) = R$3.142,46
+                        </p>
+                        
+                        <div class="feature-img">
+                           <img class="img-fluid" src="../desmistificando-o-cub/img3.png" style="max-width:100%;" alt="junte-as-suas-economias"/>
+                        </div>
+
+                        <h2 class="pt-40" style="color: #2d2d2d;">4. Agora vamos calcular o saldo devedor na entrega da obra (saldo a ser financiado)</h2>
+                        <p style="text-align:justify;">
+                           
+                        </p>
+                        
+                        <div class="feature-img">
+                           <img class="img-fluid" src="../desmistificando-o-cub/img4.png" style="max-width:100%;" alt="compare-o-metro-quadrado"/>
+                        </div>
+
+
+                        </br>Gostou?
+                        </br> Então, fique ligado que no próximo post vamos analisar: a série histórica do CUB/SC versus a taxa de juros do financiamento imobiliário.
+                        </br> E mostraremos o quanto comprar um imóvel na planta pode ser um bom negócio!
+
+                     </div>
+                  </div>
+                  </br>
+
+                  <h5 style="color:black;"><a href="#">#mercadoimobiliario</a><a href="#">#construcaocivil</a></h5>
+                  <div class="navigation-top">
+                     <div class="d-sm-flex justify-content-between text-center">
+                        <div class="col-sm-4 text-center my-2 my-sm-0">
+                        </div>
+                     </div>
+                     
+                     <div class="navigation-area">
+                        <div class="row">
+                           <div class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
+                              <div class="thumb">
+                                 <a href="/fissuras-trincas-rachaduras">
+                                    <img class="" src="../fissuras-trincas-rachaduras/CAPA.jpg" alt="fissuras-trincas-rachaduras-capa" height="80px">
+                                 </a>
+                              </div>
+                              <div class="arrow">
+                                 <a href="/fissuras-trincas-rachaduras">
+                                    <span class="lnr text-white ti-arrow-left"></span>
+                                 </a>
+                              </div>
+                              <div class="details">
+                                 <p>Post Anterior</p>
+                                 <a href="/fissuras-trincas-rachaduras">
+                                    <h4 style="color: #2d2d2d;">FISSURAS, TRINCAS e RACHADURAS quando devo me preocupar?</h4>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
+               </div>
+               <div class="col-lg-4">
+                        <div class="blog_right_sidebar">
+                            <aside class="single_sidebar_widget search_widget">
+                                <form action="#">
+                                    <div class="form-group">
+                                        <div class="input-group mb-3">
+                                            <input type="text" class="form-control" placeholder='Digite aqui o que deseja encontrar...' onfocus="this.placeholder = ''" onblur="this.placeholder = 'Digite aqui o que deseja encontrar...'">
+                                        </div>
+                                    </div>
+                                    <button class="button primary-bg text-white w-100 btn_1 boxed-btn" style="border-radius: 10px;" type="submit">Pesquisar</button>
+                                </form>
+                            </aside>
+                            <aside class="single_sidebar_widget post_category_widget">
+                                <h4 class="widget_title" style="color: #2d2d2d;">Categorias</h4>
+                                <ul class="list cat-list">
+                                <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Galvan Engenharia</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Joinville</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Construção Civil</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Engenharia e tecnologia</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Mercado imobiliário</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Arquitetura e Decoração</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Estilo de Vida</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Curiosidades</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="d-flex">
+                                            <p>Eventos/Reconhecimento</p>
+                                            <p> (01)</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </aside>
+                        </div>
+                    </div>
+            </div>
+         </div>
+      </section>
+      <!-- Blog Area End -->
+   </main>
+   <!-- Footer Start-->
+   <?php include $_SERVER["DOCUMENT_ROOT"] . "/estrutura/footer.php"; ?>
+   <!-- Footer End-->
+
+   <!-- BOTÃO DO WHATSAPP INÍCIO -->
+   <a href="https://wa.me/5547989190606" style="
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#25d366;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            font-size:30px;
+            box-shadow: 1px 1px 2px #888;
+            z-index:1000;" target="_blank">
+
+      <i style="margin-top:16px" class="fab fa-whatsapp"></i></a>
+
+   <!-- BOTÃO DO WHATSAPP FIM -->
+
+   <!-- JS here -->
+
+   <script src="../assets/js/vendor/modernizr-3.5.0.min.js"></script>
+   <!-- Jquery, Popper, Bootstrap -->
+   <script src="../assets/js/vendor/jquery-1.12.4.min.js"></script>
+   <script src="../assets/js/popper.min.js"></script>
+   <script src="../assets/js/bootstrap.min.js"></script>
+   <!-- Jquery Mobile Menu -->
+   <script src="../assets/js/jquery.slicknav.min.js"></script>
+
+   <!-- Jquery Slick , Owl-Carousel Plugins -->
+   <script src="../assets/js/owl.carousel.min.js"></script>
+   <script src="../assets/js/slick.min.js"></script>
+   <!-- One Page, Animated-HeadLin -->
+   <script src="../assets/js/wow.min.js"></script>
+   <script src="../assets/js/animated.headline.js"></script>
+   <script src="../assets/js/jquery.magnific-popup.js"></script>
+
+   <!-- Date Picker -->
+   <script src="../assets/js/gijgo.min.js"></script>
+   <!-- Nice-select, sticky -->
+   <script src="../assets/js/jquery.nice-select.min.js"></script>
+   <script src="../assets/js/jquery.sticky.js"></script>
+
+   <!-- counter , waypoint,Hover Direction -->
+   <script src="../assets/js/jquery.counterup.min.js"></script>
+   <script src="../assets/js/waypoints.min.js"></script>
+   <script src="../assets/js/jquery.countdown.min.js"></script>
+   <script src="./assets/js/hover-direction-snake.min.js"></script>
+
+   <!-- contact js -->
+   <script src="../assets/js/contact.js"></script>
+   <script src="../assets/js/jquery.form.js"></script>
+   <script src="../assets/js/jquery.validate.min.js"></script>
+   <script src="../assets/js/mail-script.js"></script>
+   <script src="../assets/js/jquery.ajaxchimp.min.js"></script>
+
+   <!-- Jquery Plugins, main Jquery -->
+   <script src="../assets/js/plugins.js"></script>
+   <script src="../assets/js/main.js"></script>
+
+
+</body>
+
+</html>
